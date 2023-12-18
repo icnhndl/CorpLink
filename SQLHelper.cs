f16c4f700e4d4bbacec09306e6a3ba247bde0c0e
icnhndl <95489259+icnhndl@users.noreply.github.com>
2023-12-18 14:03:02 +0300

    Update SQLHelper.cs

--- SQLHelper.cs
+++ SQLHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +19,9 @@ namespace CorpLink
         public void RegisterUser(string login, string password)
         {
             string passwordHash = Encoder.Encode(password);
-            string query = $"INSERT INTO users(login, password_hash) VALUES({login}, {passwordHash});";
+            string query =
+                $"INSERT INTO users(login, password_hash) " +
+                $"VALUES({login}, {passwordHash});";
             using (MySqlCommand cmd = new MySqlCommand(query, connection))
             {
                 cmd.ExecuteNonQuery();
@@ -28,7 +30,9 @@ namespace CorpLink
 
         public bool IsUserExists(string login)
         {
-            string query = $"SELECT COUNT(*) FROM users WHERE login = {login};";
+            string query =
+                $"SELECT COUNT(*) FROM users " +
+                $"WHERE login = {login};";
             using (MySqlCommand cmd = new MySqlCommand(query, connection))
             {
                 return (int)cmd.ExecuteScalar() == 1;
@@ -39,24 +43,50 @@ namespace CorpLink
         public bool AuthorizeUser(string login, string password)
         {
             string passwordHash = Encoder.Encode(password);
-            string query = $"SELECT COUNT(*) FROM users WHERE login = {login} AND password_hash = {passwordHash};";
+            string query =
+                $"SELECT COUNT(*) FROM users " +
+                $"WHERE login = {login} AND password_hash = {passwordHash};";
             using (MySqlCommand cmd = new MySqlCommand(query, connection))
             {
                 return (int)cmd.ExecuteScalar() == 1;
             }
         }
 
-        public object GetContacts(string login)
+        public List<UserDetails> GetContacts(string login)
         {
-            string query = 
-                $"SELECT * FROM contacts c" +
-                $"WHERE owner_id = {login}" +
+            string query =
+                $"SELECT * FROM contacts c " +
+                $"WHERE owner_id = {login} " +
                 $"JOIN user_details ud " +
-                $"ON c.user_id = ud.user_id;";
+                $"ON c.user_id = ud.user_id " +
+                $"JOIN departments d " +
+                $"ON ud.department_id = d.id;";
             using (MySqlCommand cmd = new MySqlCommand(query, connection))
             {
-                return (int)cmd.ExecuteScalar() == 1;
+                var contacts_data = cmd.ExecuteReader();
+
+                List<UserDetails> userDetails = new();
+
+                while (contacts_data.Read())
+                {
+                    userDetails.Add(new UserDetails
+                    {
+                        Surname = (string)contacts_data["surname"],
+                        Name = (string)contacts_data["name"],
+                        PatName = (string)contacts_data["pat_name"],
+                        OfficeNumber = (int)contacts_data["office_num"],
+                        Photo = (byte[])contacts_data["photo"],
+                        FaxNumber = (string)contacts_data["fax_num"],
+                        PersonalNumber = (string)contacts_data["personal_num"],
+                        DepartmentName = (string)contacts_data["department_name"]
+                    });
+
+                }
+                return userDetails;
             }
         }
+
+
+
     }
 }
